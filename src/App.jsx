5d2823f83ceb3bf2ce48d3955a67170bf60c6270
Breno Leite <brenoleitepereira@gmail.com>
2023-02-02 13:41:55 -0300

    First Commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,14 @@
 import Header from "./components/Header";
 import Main from "./components/Main";
-import { motion } from "framer-motion";
 import { GlobalStyle } from "./styles/globalStyles";
 
 function App() {
   return (
     <>
       <GlobalStyle />
-      <motion.div
-        initial={{ opacity: 0 }}
-        animate={{ opacity: 1 }}
-        transition={{
-          delay: 0.5,
-          x: { duration: 1 },
-          default: { ease: "linear" },
-        }}
-      >
-        <Header />
-        <Main />
-      </motion.div>
+
+      <Header />
+      <Main />
     </>
   );
 }
--- src/components/Main/index.jsx
+++ src/components/Main/index.jsx
@@ -1,29 +1,40 @@
 import mac from "../../assets/mac.jpeg";
 import { StyledDiv, StyledDivMain, StyledMain } from "./style";
+import { motion } from "framer-motion";
 
 const Main = () => {
   return (
     <StyledMain>
-      <StyledDiv>
-        <img src={mac} alt="Macbook" />
-      </StyledDiv>
+      <motion.div
+        initial={{ opacity: 0 }}
+        animate={{ opacity: 1 }}
+        transition={{
+          delay: 0.2,
+          x: { duration: 0.2 },
+          default: { ease: "easeIn" },
+        }}
+      >
+        <StyledDiv>
+          <img src={mac} alt="Macbook" />
+        </StyledDiv>
 
-      <StyledDivMain>
-        <h2>Bem vindo ao Shape.</h2>
+        <StyledDivMain>
+          <h2>Bem vindo ao Shape.</h2>
 
-        <h1>
-          Transformando ações <br /> em oportunidades
-        </h1>
+          <h1>
+            Transformando ações <br /> em oportunidades
+          </h1>
 
-        <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Recusandae
-          consequuntur necessitatibus dicta? Quibusdam quidem quae, beatae
-          perferendis repellat quas cum, temporibus maiores vel excepturi,
-          perspiciatis earum dicta esse officia voluptates?
-        </p>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Recusandae
+            consequuntur necessitatibus dicta? Quibusdam quidem quae, beatae
+            perferendis repellat quas cum, temporibus maiores vel excepturi,
+            perspiciatis earum dicta esse officia voluptates?
+          </p>
 
-        <button>Saiba mais</button>
-      </StyledDivMain>
+          <button>Saiba mais</button>
+        </StyledDivMain>
+      </motion.div>
     </StyledMain>
   );
 };
