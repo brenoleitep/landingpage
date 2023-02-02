eb7097b0236c51b697f2a0dd9226254ec768b960
Breno Leite <brenoleitepereira@gmail.com>
2023-02-02 13:21:08 -0300

    Commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,16 @@
 import Header from "./components/Header";
 import Main from "./components/Main";
+import { motion } from "framer-motion";
 import { GlobalStyle } from "./styles/globalStyles";
 
 function App() {
   return (
     <>
       <GlobalStyle />
-      <Header />
-      <Main />
+      <motion.div initial={{ opacity: -1 }} animate={{ opacity: 1 }}>
+        <Header />
+        <Main />
+      </motion.div>
     </>
   );
 }
--- src/components/Main/index.jsx
+++ src/components/Main/index.jsx
@@ -1,32 +1,29 @@
 import mac from "../../assets/mac.jpeg";
-import { motion } from "framer-motion";
 import { StyledDiv, StyledDivMain, StyledMain } from "./style";
 
 const Main = () => {
   return (
     <StyledMain>
-      <motion.div initial={{ opacity: 0 }} animate={{ opacity: 1 }}>
-        <StyledDiv>
-          <img src={mac} alt="Macbook" />
-        </StyledDiv>
+      <StyledDiv>
+        <img src={mac} alt="Macbook" />
+      </StyledDiv>
 
-        <StyledDivMain>
-          <h2>Bem vindo ao Shape.</h2>
+      <StyledDivMain>
+        <h2>Bem vindo ao Shape.</h2>
 
-          <h1>
-            Transformando ações <br /> em oportunidades
-          </h1>
+        <h1>
+          Transformando ações <br /> em oportunidades
+        </h1>
 
-          <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Recusandae
-            consequuntur necessitatibus dicta? Quibusdam quidem quae, beatae
-            perferendis repellat quas cum, temporibus maiores vel excepturi,
-            perspiciatis earum dicta esse officia voluptates?
-          </p>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Recusandae
+          consequuntur necessitatibus dicta? Quibusdam quidem quae, beatae
+          perferendis repellat quas cum, temporibus maiores vel excepturi,
+          perspiciatis earum dicta esse officia voluptates?
+        </p>
 
-          <button>Saiba mais</button>
-        </StyledDivMain>
-      </motion.div>
+        <button>Saiba mais</button>
+      </StyledDivMain>
     </StyledMain>
   );
 };
